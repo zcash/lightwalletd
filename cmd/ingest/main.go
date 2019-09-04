b4070be5b937a1da3437eca77365896eca3db4a4
Danila Samosudov <samosudovd@gmail.com>
2019-09-04 15:12:56 +0300

    no new variables on left side of :=

--- cmd/ingest/main.go
+++ cmd/ingest/main.go
@@ -222,7 +222,7 @@ func handleBlock(db *sql.DB, block *parser.Block) {
 	nodeBlockHash := hex.EncodeToString(block.GetEncodableHash())
 
 	nodeBlockHeight := block.GetHeight()
-	localBlockHash, err := storage.GetBlockHash(db, nodeBlockHeight)
+	localBlockHash, localBlockHashError := storage.GetBlockHash(db, nodeBlockHeight)
 
 	// if local block has different hash delete it before store new block
 	if localBlockHash != "" && localBlockHash != nodeBlockHash {
