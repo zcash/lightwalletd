b7e3e50293214dfdfc55a28130b0128076849ed8
Ben Wilson <ben@z.cash>
2019-12-18 16:11:05 -0500

    fixed broken cert script

--- docker/gen_cert.sh
+++ docker/gen_cert.sh
@@ -3,5 +3,4 @@
 ## In debian
 # apt-get update && apt-get install -y openssl
 
-openssl req -x509 -nodes -newkey rsa:2048 -keyout ./cert.key -out ./cert.pem -days 3650 -subj "/C=US/ST=CA/O=MyOrg, Inc./CN=lightwalletd.testnet.local" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=DN
-S:lightwalletd.testnet.local,DNS:127.0.0.1,DNS:localhost"))
+openssl req -x509 -nodes -newkey rsa:2048 -keyout ./cert.key -out ./cert.pem -days 3650 -subj "/C=US/ST=CA/O=MyOrg, Inc./CN=lightwalletd.testnet.local" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=DNS:lightwalletd.testnet.local,DNS:127.0.0.1,DNS:localhost"))
