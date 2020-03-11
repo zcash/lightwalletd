2a35592e679ee85d496e95013e1386982c3bdb70
Taylor Hornby <taylor@defuse.ca>
2020-03-11 10:00:13 -0600

    Add scripts to assist with controlling darksidewalletd

---
+++ utils/submitblocks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Submits a list of blocks, one per line in the file, to darksidewalletd.
+# Usage: ./submitblocks.sh <start height> <sapling activation> <file>
+#   e.g. ./submitblocks.sh 1000 1000 blocks.txt
+set -e
+
+JSON="{\"startHeight\": $1, \"saplingActivation\": $2, \"branchID\": \"2bb40e60\", \"chainName\": \"main\", \"blocks\": "
+JSON="$JSON$(cat "$3" | sed 's/^/"/' | sed 's/$/"/' | sed '1s/^/[/;$!s/$/,/;$s/$/]/')"
+JSON="$JSON}"
+
+grpcurl -insecure -import-path ./walletrpc/ -proto service.proto -d "$JSON" localhost:9067 cash.z.wallet.sdk.rpc.CompactTxStreamer/DarksideSetState
