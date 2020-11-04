05c51935ad375380d0dfef9015fa2e0a1d4ad958
Larry Ruane <larry@electriccoin.co>
2020-11-03 17:25:42 -0700

    fix pullblocks.sh for macOS

--- utils/pullblocks.sh
+++ utils/pullblocks.sh
@@ -2,6 +2,9 @@
 # Usage: ./pullblocks.sh 500000 500100 > blocks.txt
 test $# -ne 2 && { echo usage: $0 start end;exit 1;}
 
-for i in $(seq $1 $2); do
-	zcash-cli getblock $i 0
+let i=$1
+while test $i -le $2
+do
+    zcash-cli getblock $i 0
+    let i++
 done
